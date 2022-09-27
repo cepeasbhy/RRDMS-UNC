040c20c83a90bffec306cd2458c06430839f6d8a
asbhycepe <ashby.cepe@unc.edu.ph>
2022-09-27 20:30:19 +0800

    Added unc logo, css styling, and  modified html contents for app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/css/main.css')}}">
     <title>RRDM-UNC</title>
 </head>
 <body>
-    <div class="container">
+    <header>
+        <div id="header-wrapper">
+            <div id="header-logo">
+                <img src="{{asset('/img/unc-logo.png')}}" width="50px" height="50px">
+                <h2 id="word-logo">RRDMS-UNC</h2>
+            </div>
+            <div id="header-nav">
+
+            </div>
+        </div>
+    </header>
+    <div id="container">
         @yield('content')
     </div>
 </body>
