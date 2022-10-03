30ad0c55fd311af9326733dd2b08f53e3a680155
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-03 18:37:44 +0800

    Added new function for submitting form data to database

--- app/Http/Controllers/StudentCredential.php
+++ app/Http/Controllers/StudentCredential.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentCredential extends Controller
 {
@@ -10,7 +11,23 @@ class StudentCredential extends Controller
         return view('StudentCredential/index');
     }
 
-    public function create(){
+    public function addStudent(){
         return view('StudentCredential/add_stud');
+    }
+
+    public function create(){
+        $student = new Student();
+
+        $student->student_id = request('studentID');
+        $student->first_name = request('firstName');
+        $student->last_name = request('lastName');
+        $student->middle_name = request('middleName');
+        $student->department_id = request('department');
+        $student->course_id = request('course');
+        $student->admission_year = request('addmissionYear');
+
+        $student->save();
+
+        return redirect('/stud_cred_mngmnt');
     }
 }
