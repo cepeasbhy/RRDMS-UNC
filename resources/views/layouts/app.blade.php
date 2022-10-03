3180f2407f008ab0873616153dbc7419b4808752
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-03 14:53:37 +0800

    Added react js script link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @viteReactRefresh
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{asset('/css/main.css')}}">
     @yield('css-link')
     <title>RRDM-UNC</title>
