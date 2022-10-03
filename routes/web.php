9f0acc7b5b9895afddeadac0941b894ebae286ce
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-03 22:09:58 +0800

    Added a new route for viewing student info

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/stud_cred_mngmnt',[StudentCredential::class, 'index']);
 Route::get('/stud_cred_mngmnt/add_student',[StudentCredential::class, 'addStudent']);
+Route::get('/stud_cred_mngmnt/view_student/{id}',[StudentCredential::class, 'view']);
 Route::post('/stud_cred_mngmnt/add_student', [StudentCredential::class, 'create']);
