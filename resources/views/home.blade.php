a4a4d204bd05b66cddfe59769e6f4621661d3f65
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-03 14:47:29 +0800

    Added navigation links for each operation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,4 +11,21 @@
             <h1 id="home-title">REGISTRAR RECORS AND DOCUMENT MANAGEMENT SYSTEM</h1>
         </div>
     </div>
+    <div class="transaction-block">
+        <div class="transac-item">
+            <a href="#">
+                <img src="{{asset('img/option1.jpg')}}" width="250px" height="250px">
+            </a>
+        </div>
+        <div class="transac-item">
+            <a href="#">
+                <img src="{{asset('img/option3.jpg')}}" width="250px" height="250px">
+            </a>
+        </div>
+        <div class="transac-item">
+            <a href="#">
+                <img src="{{asset('img/option2.jpg')}}" width="250px" height="250px">
+            </a>
+        </div>
+    </div>
 @endsection
