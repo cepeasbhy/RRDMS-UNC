109647aca61df6568a5a4bc9ba08ee32b6987209
rttn-Mango <khimmanga445@gmail.com>
2022-10-06 20:24:25 +0800

    Removed the home button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @viteReactRefresh
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('/css/main.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
     @yield('css-link')
     <title>RRDM-UNC</title>
 </head>
+
 <body>
     <header class="mb-4">
         <nav class="navbar navbar-expand">
             <div class="container w-75">
                 <div class="navbar-brand d-flex align-items-center">
-                    <img src="{{asset('img/unc-logo.png')}}" width="50px" height="50px">
+                    <a href="/">
+                        <img src="{{ asset('img/unc-logo.png') }}" width="50px" height="50px">
+                    </a>
                     <div>
                         <span class="h3 ms-2">UNC-RRDMS</span>
                     </div>
                 </div>
                 <ul class="navbar-nav list-unstyled">
-                    <li class="nav-item">
-                        <a class="btn btn-sm btn-outline-primary" href="/">HOME</a>
+                    <li class="nav-item ps-2">
+                        <a class="btn btn-sm btn-outline-primary" href="{{ route('login') }}">LOGIN</a>
                     </li>
                     <li class="nav-item ps-2">
-                        <a class="btn btn-sm btn-outline-primary" href="{{route('login')}}">LOGIN</a>
-                    </li>
-                    <li class="nav-item ps-2">
-                        <a class="btn btn-sm btn-outline-primary" href="{{route('register')}}">REGISTER</a>
+                        <a class="btn btn-sm btn-outline-primary" href="{{ route('register') }}">REGISTER</a>
                     </li>
                 </ul>
             </div>
@@ -39,4 +41,5 @@
         @yield('content')
     </div>
 </body>
+
 </html>
