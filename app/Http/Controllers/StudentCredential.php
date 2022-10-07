00f1e05a5a612d66ce08fe650e6e1c8ddf194804
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-07 13:20:15 +0800

    Added function for sorting students

--- app/Http/Controllers/StudentCredential.php
+++ app/Http/Controllers/StudentCredential.php
@@ -14,7 +14,6 @@ class StudentCredential extends Controller
             'last_name',
             'dept_name',
             'course_name',
-            'admission_year'
         )->join(
             'departments', 'departments.department_id', '=', 'students.department_id'
         )->join(
@@ -46,6 +45,22 @@ class StudentCredential extends Controller
 
     public function search(){
         return view('StudentCredential/view_stud', ['student' => $this->findStudent(request('studentID'))]);
+    }
+
+    public function sort(){
+        $students = Student::select(
+            'student_id',
+            'first_name',
+            'last_name',
+            'dept_name',
+            'course_name',
+        )->join(
+            'departments', 'departments.department_id', '=', 'students.department_id'
+        )->join(
+            'courses', 'courses.course_id', '=', 'students.course_id'
+        )->orderBy(request('sort'), 'ASC')->get();
+
+        return view('StudentCredential/index', ['students' => $students]);
     }
 
     public function viewStudent($id){
