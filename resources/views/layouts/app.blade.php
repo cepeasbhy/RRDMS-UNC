efe6306078876a9eda64f2cf6d386d00b5b1c7f5
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-13 14:11:48 +0800

    Modified nav to display diff button based on logged in status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,12 +28,33 @@
                     </div>
                 </div>
                 <ul class="navbar-nav list-unstyled">
-                    <li class="nav-item ps-2">
-                        <a class="btn btn-sm btn-outline-primary" href="{{ route('login') }}">LOGIN</a>
-                    </li>
-                    <li class="nav-item ps-2">
-                        <a class="btn btn-sm btn-outline-primary" href="{{ route('register') }}">REGISTER</a>
-                    </li>
+                    @guest
+                        <li class="nav-item ps-2">
+                            <a class="btn btn-sm btn-outline-primary" href="{{ route('login') }}">LOGIN</a>
+                        </li>
+                        <li class="nav-item ps-2">
+                            <a class="btn btn-sm btn-outline-primary" href="{{ route('register') }}">REGISTER</a>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->first_name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                    
                 </ul>
             </div>
         </nav>
