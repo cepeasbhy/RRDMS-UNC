a9e4fd3ffddf548d070ea87f77b5982b2d3181e2
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-13 13:53:41 +0800

    Modified the key names

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,9 +66,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'first_name' => $data['firstName'],
-            'last_name' => $data['lastName'],
-            'faculty_id' => $data['facultyID'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'faculty_id' => $data['faculty_id'],
             'password' => Hash::make($data['password']),
         ]);
     }
