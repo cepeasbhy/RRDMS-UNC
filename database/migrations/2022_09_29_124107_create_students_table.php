59c95a44671959f8e7e215a567508071568523f8
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-16 18:08:44 +0800

    Made course nullable

--- database/migrations/2022_09_29_124107_create_students_table.php
+++ database/migrations/2022_09_29_124107_create_students_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('student_id')->unique();
             $table->string('department_id');
-            $table->string('course_id');
+            $table->string('course_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name');
