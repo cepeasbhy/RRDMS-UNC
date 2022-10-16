6bdf19a10469aacfffc129d7da224a84cf621e8d
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-16 23:22:26 +0800

    Added fields for credentials and modified layout for student information field

--- resources/views/StudentCredential/add_stud.blade.php
+++ resources/views/StudentCredential/add_stud.blade.php
@@ -5,16 +5,15 @@
         <form class="mb-3" action="/stud_cred_mngmnt" method="get">
             <button class="btn btn-success btn-sm"><i class="bi bi-arrow-bar-left"></i> BACK</button>
         </form>
-        <div class="col">
-            <div class="border-start border-danger border-4">
-                <h4 class="ms-3">STUDENT INFORMATION</h4>
-            </div>
-            <div class="ms-2">
-                <form action="/stud_cred_mngmnt/add_student" method="POST">
-                    @csrf
-                    <div class="form-group row mb-2">
-                        <label class="col-sm-3 col-form-label col-form-label-sm" for="">Student ID:</label>
-                        <div class="col-sm-9">
+        <form class="row" action="/stud_cred_mngmnt/add_student" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="col-5">
+                <div class="border-start border-danger border-4">
+                    <h4 class="ms-3">STUDENT INFORMATION</h4>
+                </div>
+                <div class="ms-2">
+                        <div class="form-group mb-2">
+                            <label class=" col-form-label col-form-label-sm" for="">Student ID:</label>
                             <input class="form-control form-control-sm @error('student_id') is-invalid @enderror" value="{{old('student_id')}}"type="text" name="student_id" required>
                             @error('student_id')
                                 <span class="invalid-feedback" role="alert">
@@ -22,10 +21,8 @@
                                 </span>
                             @enderror
                         </div>
-                    </div>
-                    <div class="form-group row mb-2">
-                        <label class="col-sm-3 col-form-label col-form-label-sm" for="">First Name</label>
-                        <div class="col-sm-9">
+                        <div class="form-group mb-2">
+                            <label class="col-form-label col-form-label-sm" for="">First Name</label>
                             <input class="form-control form-control-sm @error('first_name') is-invalid @enderror" type="text" value="{{old('first_name')}}" name="first_name" required>
                             @error('first_name')
                                 <span class="invalid-feedback" role="alert">
@@ -33,10 +30,8 @@
                                 </span>
                             @enderror
                         </div>
-                    </div>
-                    <div class="form-group row mb-2">
-                        <label class="col-sm-3 col-form-label col-form-label-sm" for="">Last Name</label>
-                        <div class="col-sm-9">
+                        <div class="form-group mb-2">
+                            <label class="col-form-label col-form-label-sm" for="">Last Name</label>
                             <input class="form-control form-control-sm @error('last_name') is-invalid @enderror" type="text" value="{{old('last_name')}}" name="last_name" required>
                             @error('last_name')
                                 <span class="invalid-feedback" role="alert">
@@ -44,10 +39,8 @@
                                 </span>
                             @enderror
                         </div>
-                    </div>
-                    <div class="form-group row mb-2">
-                        <label class="col-sm-3 col-form-label col-form-label-sm" for="">Middle Name</label>
-                        <div class="col-sm-9">
+                        <div class="form-group mb-2">
+                            <label class="col-form-label col-form-label-sm" for="">Middle Name</label>
                             <input class="form-control form-control-sm @error('middle_name') is-invalid @enderror" type="text" value="{{old('middle_name')}}" name="middle_name" required>
                             @error('middle_name')
                                 <span class="invalid-feedback" role="alert">
@@ -55,31 +48,83 @@
                                 </span>
                             @enderror
                         </div>
-                    </div>
-                    <div class="form-group row mb-2" id="selection">
-        
-                    </div>
-                    <div class="form-group row mb-2">
-                        <label class="col-sm-3 col-form-label col-form-label-sm" for="">Admission Year</label>
-                        <div class="col-sm-9">
+                        <div class="form-group mb-2" id="selection">
+            
+                        </div>
+                        <div class="form-group mb-2">
+                            <label class="col-form-label col-form-label-sm" for="">Admission Year</label>
                             <input class="form-control form-control-sm @error('admission_year') is-invalid @enderror" type="text" value="{{old('admission_year')}}" name="admission_year" required>
                             @error('admission_year')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                             @enderror
+                            @enderror
                         </div>
-                    </div>
-                    <div class="form-group row mb-2">
-                        <input class="btn btn-sm btn-success"type="submit" value="ADD STUDENT">
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="border-start border-danger border-4">
-                <h4 class="ms-3">STUDENT CREDENTIALS</h4>
+            <div class="col">
+                <div class="border-start border-danger border-4">
+                    <h4 class="ms-3">STUDENT CREDENTIALS</h4>
+                </div>
+                <div class="ms-2">
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="picture">2x2 Picture</label>
+                            <input id="picture" class="form-control form-control-sm" type="file" name="picture">
+                        </div>
+                        <div class="form-group row mb-2">
+                            <div class="col">
+                                <label class="col-form-label-sm" for="birthCertificate">Birth Certificate</label>
+                            <input id="birthCertificate" class="form-control form-control-sm" type="file" name="birthCertificate">
+                            </div>
+                            <div class="col">
+                                <label class="col-form-label-sm" for="marriageCertificate">Marriage Certificate</label>
+                                <input id="marriageCertificate" class="form-control form-control-sm" type="file" name="marriageCertificate">
+                            </div>
+                        </div>
+                        
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="goodMoralCharacter">Certificate of Good Moral Character</label>
+                            <input id="goodMoralCharacter" class="form-control form-control-sm" type="file" name="goodMoralCharacter">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="honorDismisal">Honarable Dismisal</label>
+                            <input id="honorDismisal" class="form-control form-control-sm" type="file" name="honorDismisal">
+                        </div>
+                        <div class="form-group mb-2 row">
+                            <div class="col">
+                                <label class="col-form-label-sm" for="form-138">Form 138</label>
+                                <input id="form138" class="form-control form-control-sm" type="file" name="form138">
+                            </div>
+                            <div class="col">
+                                <label class="col-form-label-sm" for="form137">Form 137</label>
+                                <input id="form137" class="form-control form-control-sm" type="file" name="form137">
+                            </div>
+                            <div class="col">
+                                <label class="col-form-label-sm" for="copyGrade">Copy of Grades</label>
+                                <input id="copyGrade" class="form-control form-control-sm" type="file" name="copyGrade">
+                            </div>
+                        </div>
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="tor">Transcript of Record</label>
+                            <input id="tor" class="form-control form-control-sm" type="file" name="tor">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="clearance">NBI/Police Clearance</label>
+                            <input id="clearance" class="form-control form-control-sm" type="file" name="clearance">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="C1">C1 Official Receipt</label>
+                            <input id="C1" class="form-control form-control-sm" type="file" name="C1">
+                        </div>
+                        <div class="form-group mb-2">
+                            <label class="col-form-label-sm" for="permitCrossEnroll">Permit to Cross Enroll</label>
+                            <input id="permitCrossEnroll" class="form-control form-control-sm" type="file" name="permitCrossEnroll">
+                        </div>
+                </div>
             </div>
-        </div>
+            <div class="form-group mt-3 mb-5 text-center">
+                <button class="w-50 btn btn-success">ADD STUDENT</button>
+            </div>
+        </form>
     </div>
 @endsection
