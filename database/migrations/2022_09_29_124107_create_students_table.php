f81665cf38c27bc28b98c4ec1c549c6859988709
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-18 22:10:43 +0800

    Added archive_status column in students table

--- database/migrations/2022_09_29_124107_create_students_table.php
+++ database/migrations/2022_09_29_124107_create_students_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name');
+            $table->integer('archive_status')->default(0);
             $table->integer('admission_year');
             $table->timestamps();
 
