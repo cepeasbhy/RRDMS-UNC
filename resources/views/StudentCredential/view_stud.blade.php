8329561098763b12672e107038b71e6456b18431
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-18 20:46:16 +0800

    Removed html modal and moved them to separate bladde files

--- resources/views/StudentCredential/view_stud.blade.php
+++ resources/views/StudentCredential/view_stud.blade.php
@@ -67,87 +67,10 @@
             </div>
         </div>
         <!--Modal for Updating Student Information-->
-        <div id="update-modal" class="modal fade" tabindex="-1" aria-labelledby="title-modal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="title-modal">Update Student Information</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form id="update-form" action="{{route('updateStudent', ['id' => $student->student_id])}}" method="post">
-                            @csrf
-                            <div class="form-group mb-2">
-                                <label class="col-form-label col-form-label-sm" for="">First Name</label>
-                                    <input class="form-control form-control-sm @error('first_name') is-invalid @enderror" type="text" name="first_name" value="{{$student->first_name}}">
-                                    @error('first_name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                            </div>
-                            <div class="form-group mb-2">
-                                <label class="col-form-label col-form-label-sm" for="">Last Name</label>
-                                <input class="form-control form-control-sm @error('last_name') is-invalid @enderror" type="text" name="last_name" value="{{$student->last_name}}">
-                                @error('last_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group mb-2">
-                                <label class="col-form-label col-form-label-sm" for="">Middle Name</label>
-                                <input class="form-control form-control-sm @error('middle_name') is-invalid @enderror" type="text" name="middle_name" value="{{$student->middle_name}}">
-                                @error('middle_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group mb-2" id="selection">
-        
-                            </div>
-                            <div class="form-group mb-2">
-                                <label class="col-form-label col-form-label-sm" for="">Admission Year</label>
-                                <input class="form-control form-control-sm @error('admission_year') is-invalid @enderror" type="text" name="admission_year" value="{{$student->admission_year}}">
-                                @error('admission_year')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-sm btn-success" form="update-form">Update Information</button>
-                        <button class="btn btn-sm btn-danger" data-bs-dismiss="modal">Cancel</button>
-                    </div>
-                </div>
-            </div>
-       </div>
-       <!--Modal for Deleting Student-->
-       <div id="delete-modal" class="modal fade" tabindex="-1" aria-labelledby="title-modal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="title-modal">Remove Student from Records</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>
-                        Are you sure you want to remove this student from the records?
-                    </p>
-                </div>
-                <div class="modal-footer">
-                    <form action="{{route('deleteStudent', ['id' => $student->student_id])}}" method="post">
-                        @csrf
-                        <button class="btn btn-sm btn-danger">Proceed</button>
-                    </form>
-                    <button class="btn btn-sm btn-success" data-bs-dismiss="modal">Cancel</button>
-                </div>
-            </div>
-        </div>
-    </div>
+        @extends('layouts.modals.StudentCredential.updateModal')
+        <!--Modal for Deleting Student-->
+        @extends('layouts.modals.StudentCredential.deleteModal')
+
     @if(Session::has('errors'))
         <script>
             window.onload = function(){
