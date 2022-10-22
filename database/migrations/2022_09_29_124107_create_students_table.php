b7cd871eedcc25f95b0057dfc36bb4767e73a5bd
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-22 16:46:54 +0800

    Modified the columns for students table

--- database/migrations/2022_09_29_124107_create_students_table.php
+++ database/migrations/2022_09_29_124107_create_students_table.php
@@ -18,9 +18,6 @@ return new class extends Migration
             $table->string('student_id')->unique();
             $table->string('department_id');
             $table->string('course_id')->nullable();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('middle_name')->nullable();
             $table->integer('archive_status')->default(0);
             $table->integer('admission_year');
             $table->timestamps();
