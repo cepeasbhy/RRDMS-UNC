b55ab173999e3073ae4849584ecdb26d513a6a04
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-22 17:33:56 +0800

    Added middle name in the fillables

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
+        'middle_name',
         'user_id',
         'account_role',
         'email',
