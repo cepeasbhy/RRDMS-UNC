d5a476b7773ef6697625d1ca6e66a93acbb4a412
asbhycepe <ashby.cepe@unc.edu.ph>
2022-10-22 17:56:39 +0800

    Added new field for registering staffs

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-3">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
@@ -15,16 +15,15 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
                         <div class="row mb-3">
-                            <label for="accountRole" class="col-md-4 col-form-label text-md-end">{{ __('Account Type') }}</label>
+                            <label for="accountRole" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Account Type') }}</label>
 
                             <div class="col-md-6">
                                <select class="form-select form-select-sm @error('accountRole') is-invalid @enderror" name="account_role" id="accountRole" required>
-                                    <option disabled selected>Choose...</option>
+                                    <option selected>Choose...</option>
                                     <option value="ADMIN">Admin</option>
                                     <option value="RECORD_ASSOCIATE">Record Associate</option>
                                     <option value="EVALUATOR">Evaluator</option>
                                </select>
-
                                 @error('accountRole')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -33,7 +32,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="userID" class="col-md-4 col-form-label text-md-end">{{ __('User ID') }}</label>
+                            <label for="userID" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('User ID') }}</label>
 
                             <div class="col-md-6">
                                 <input id="userID" type="text" class="form-control form-control-sm @error('user_id') is-invalid @enderror" name="user_id" value="{{ old('user_id') }}" required>
@@ -46,7 +45,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="firstName" class="col-md-4 col-form-label text-md-end">{{ __('First Name') }}</label>
+                            <label for="firstName" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('First Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="firstName" type="text" class="form-control form-control-sm @error('firstName') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required>
@@ -59,7 +58,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="lastName" class="col-md-4 col-form-label text-md-end">{{ __('Last Name') }}</label>
+                            <label for="lastName" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Last Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="lastName" type="text" class="form-control form-control-sm @error('lastName') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required>
@@ -72,7 +71,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="middleName" class="col-md-4 col-form-label text-md-end">{{ __('Middle Name') }}</label>
+                            <label for="middleName" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Middle Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="middleName" type="text" class="form-control form-control-sm @error('middleName') is-invalid @enderror" name="middle_name" value="{{ old('middle_name') }}" required>
@@ -85,7 +84,31 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
+                            <label for="email" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Assigned Department') }}</label>
+
+                            <div class="col-md-6">
+                                <select class="form-select form-select-sm " name="assigned_dept">
+                                    <option selected>Choose...</option>
+                                    <option value="001">Arts and Science</option>
+                                    <option value="002">Business and Accountancy</option>
+                                    <option value="003">Computer Studies</option>
+                                    <option value="004">Criminal Justice Education</option>
+                                    <option value="005">Education</option>
+                                    <option value="006">Engineering and Architecture</option>
+                                    <option value="007">Nursing</option>
+                                    <option value="008">Graduate Studies</option>
+                                    <option value="009">School of Law</option>
+                                </select>
+
+                                @error('middleName')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control form-control-sm @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required>
@@ -98,7 +121,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control form-control-sm @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -112,7 +135,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label col-form-label-sm text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control form-control-sm" name="password_confirmation" required autocomplete="new-password">
