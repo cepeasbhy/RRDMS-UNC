67f0a021e05581e6aff0d6749f94a32d5b211a1a
asbhycepe <ashby.cepe@unc.edu.ph>
2022-11-03 11:19:48 +0800

    Added a constructor in checking if user is logged in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,25 @@
     <div class="container">
         <h2 class="text-center">REGISTRAR RECORS AND DOCUMENT MANAGEMENT SYSTEM</h2>
     </div>
-    <div class="row">
-        <div class="col">
+    <div class="row justify-content-center">
+        <div class="col-4">
             <a href="/stud_cred_mngmnt">
                 <img src="{{ asset('img/option1.jpg') }}" width="250px" height="250px">
             </a>
         </div>
-        <div class="col">
-            <a href="/archived_records">
-                <img src="{{ asset('img/option3.jpg') }}" width="250px" height="250px">
-            </a>
-        </div>
-        <div class="col">
-            <a href="/request">
-                <img src="{{ asset('img/option2.jpg') }}" width="250px" height="250px">
-            </a>
-        </div>
+        @if (Auth::user()->account_role == 'RECORD_ASSOCIATE')
+            <div class="col-4">
+                <a href="/archived_records">
+                    <img src="{{ asset('img/option3.jpg') }}" width="250px" height="250px">
+                </a>
+            </div>
+        @endif
+        @if (Auth::user()->account_role == 'CIC')
+            <div class="col-4">
+                <a href="/request">
+                    <img src="{{ asset('img/option2.jpg') }}" width="250px" height="250px">
+                </a>
+            </div>
+        @endif
     </div>
 @endsection
