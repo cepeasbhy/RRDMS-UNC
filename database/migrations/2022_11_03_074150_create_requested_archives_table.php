95f1a9ccb0c7ccab21ffbd627aeb4000bc4600b4
asbhycepe <ashby.cepe@unc.edu.ph>
2022-11-03 16:10:58 +0800

    Added new migration table for requesting archives

---
+++ database/migrations/2022_11_03_074150_create_requested_archives_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requested_archives', function (Blueprint $table) {
+            $table->id();
+            $table->string('request_id');
+            $table->string('staff_id');
+            $table->string('archive_id');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+
+            $table->foreign('staff_id')->references('staff_id')->on('staff');
+            $table->foreign('archive_id')->references('archive_id')->on('archives');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requested_archives');
+    }
+};
