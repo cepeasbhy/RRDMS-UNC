62f71026a415f59c551f150a687bf6f57f29c1c8
asbhycepe <ashby.cepe@unc.edu.ph>
2023-01-10 10:09:55 +0800

    Added new column name activated_status for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone_number')->nullable();
             $table->string('address')->nullable();
+            $table->integer('activated_status')->default(1);
             $table->string('account_role');
             $table->dateTime('change_pass_at')->nullable();
             $table->string('password')->default(Hash::make('welcometounc'));
