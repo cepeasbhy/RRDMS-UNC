60c3592ac74eb676eaf52fcabe509058ef4d1394
rttn-Mango <khimmanga445@gmail.com>
2023-04-18 00:22:48 +0800

    Converted login page styling to vanilla css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="mt-5">
-    <div class="row justify-content-center">
-        <div class="col-6">
-            <div class="col mb-3 d-flex justify-content-center">
-                <img src="{{asset('/img/unc-logo.png')}}" height="100%" width="100px">
-            </div>
-            <div class="col mb-4">
-                <h3 class="text-center">Registrar Records and Document Management System</h3>
-            </div>
-            <div class="card">
-                <div class="card-header fw-bold">ACCOUNT LOGIN</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="row mb-3">
-                            <label for="user_id" class="col-md-4 col-form-label text-md-end">User ID</label>
-
-                            <div class="col-md-6">
-                                <div class="input-group">
-                                    <span class="input-group-text">
-                                        <i class="bi bi-person-circle"></i>
-                                    </span>
-                                    <input id="user_id" type="text" class="form-control @error('user_id') is-invalid @enderror" name="user_id" value="{{ old('user_id') }}" required>
-                                    @error('user_id')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <div class="input-group">
-                                    <span class="input-group-text">
-                                        <i class="bi bi-key-fill"></i>
-                                    </span>
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col-md-8">
-                                <button type="submit" class="btn btn-danger fw-bold btn-sm w-100">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <article class="view-container">
+        <div class="unc-logo">
+            <img src="{{ asset('/img/unc-logo.png') }}" height="100%" width="100px">
         </div>
-    </div>
-</div>
+        <h2>Registrar Records and Document Management System</h2>
+
+        <section class="form-group">
+            <div class="form-group-header">
+                <h5>ACCOUNT LOGIN</h5>
+            </div>
+
+            <div class="form-content">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <label for="user_id">User ID</label>
+                    <div class="input-container">
+                        <span class="input-icon">
+                            <i class="bi bi-person-circle"></i>
+                        </span>
+                        <input id="user_id" type="text"
+                            class="input-box form-control @error('user_id') is-invalid @enderror" name="user_id"
+                            value="{{ old('user_id') }}" required>
+                        @error('user_id')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <label for="password" class="">{{ __('Password') }}</label>
+                    <div class="input-container">
+                        <span class="input-icon">
+                            <i class="bi bi-key-fill"></i>
+                        </span>
+                        <input id="password" type="password"
+                            class="input-box form-control @error('password') is-invalid @enderror" name="password" required
+                            autocomplete="current-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="form-button-container">
+                        <button type="submit" class="form-button">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+            </div>
+
+
+
+            </form>
+            </div>
+        </section>
+
+    </article>
 @endsection
