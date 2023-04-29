6b956b6d4921c896f1a7d96a0d96e3c441573dad
rttn-Mango <khimmanga445@gmail.com>
2023-04-29 21:05:35 +0800

    Fixed student credential management module view for cic

--- resources/views/StudentCredential/index.blade.php
+++ resources/views/StudentCredential/index.blade.php
@@ -6,73 +6,76 @@
 @endsection
 
 @section('content')
-    <div class="my-3 row align-items-center">
-        @if (Auth::user()->account_role == 'rec_assoc')
-            <div class="col-sm-9">
-            @else
-                <div class="col-sm-6">
-        @endif
-        <h3>Student Credential Management</h3>
-        <span class="badge bg-success mb-2">{{ session('msg') }}</span>
-    </div>
-    @if (Auth::user()->account_role == 'cic')
-        <div class="col-sm-3">
-            <form class="w-100" action="{{ route('addStudent') }}" method="get">
-                <input class="w-100 btn btn-sm btn-success" type="submit" value="ADD STUDENT">
-            </form>
+    <section class="view-container">
+        <div class="grid-container student-lists form-content">
+            <div class="shead">
+                <h2>Student Credential Management</h2>
+                <span class="badge bg-success mb-2">{{ session('msg') }}</span>
+            </div>
+            <div class="grid-container cic-buttons">
+                @if (Auth::user()->account_role == 'cic')
+                    <div>
+                        <form style="margin: 0" action="{{ route('addStudent') }}" method="get">
+                            <input class="print" type="submit" value="ADD STUDENT">
+                        </form>
+                    </div>
+                    <div>
+                        <form style="margin: 0" action="{{ route('requestArchive') }}" method="get">
+                            <input class="cancel req-from-archives" type="submit" value="REQUEST FROM ARCHIVES">
+                        </form>
+                    </div>
+                @endif
+            </div>
         </div>
-        <div class="col-sm-3">
-            <form class="w-100" action="{{ route('requestArchive') }}" method="get">
-                <input class="w-100 btn btn-sm btn-danger" type="submit" value="REQUEST FROM ARCHIVES">
-            </form>
-        </div>
-    @endif
-    </div>
 
-    <div class="container my-3">
-        <table id="myTable" class="table" style="width: 100%">
-            <thead>
-                <th class="custom-th bg-danger">STUDENT ID</th>
-                <th class="custom-th bg-danger">FIRST NAME</th>
-                <th class="custom-th bg-danger">LAST NAME</th>
-                <th class="custom-th bg-danger">DEPARTMENT</th>
-                <th class="custom-th bg-danger">PROGRAM</th>
-                <th class="custom-th bg-danger">ACTION</th>
-            </thead>
-            <tbody>
-                @foreach ($students as $student)
-                    <tr class="custom-tr">
-                        <td class="custom-td">{{ $student->student_id }}</td>
-                        <td class="custom-td">{{ $student->first_name }}</td>
-                        <td class="custom-td">{{ $student->last_name }}</td>
-                        <td class="custom-td">{{ $student->dept_name }}</td>
-                        <td class="custom-td">{{ $student->course_name }}</td>
-                        <td class="custom-td">
-                            <form action="{{ route('viewStudent', ['id' => $student->student_id]) }}" method="GET">
-                                @csrf
-                                <input type="submit" value="VIEW" class="btn btn-success btn-sm">
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-            integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function() {
-                $('#myTable').DataTable({
-                    "language": {
-                        "lengthMenu": "Display _MENU_ records per page",
-                        "zeroRecords": "No Records Available",
-                        "info": "Showing page _PAGE_ of _PAGES_",
-                        "infoEmpty": "No records available",
-                        "infoFiltered": "(filtered from _MAX_ total records)"
-                    }
+        <div class="tab-container" style="min-width: 80%; border-top: 0">
+            <div class="tab-container__contents active">
+                <table>
+                    <thead>
+                        <th class="table-header">STUDENT ID</th>
+                        <th class="table-header">FIRST NAME</th>
+                        <th class="table-header">LAST NAME</th>
+                        <th class="table-header">DEPARTMENT</th>
+                        <th class="table-header">PROGRAM</th>
+                        <th class="table-header">ACTION</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($students as $student)
+                            <tr class="custom-tr">
+                                <td data-label="Student Id:">{{ $student->student_id }}</td>
+                                <td data-label="First Name:">{{ $student->first_name }}</td>
+                                <td data-label="Last Name:">{{ $student->last_name }}</td>
+                                <td data-label="Department:">{{ $student->dept_name }}</td>
+                                <td data-label="Program:">{{ $student->course_name }}</td>
+                                <td data-label="Action:">
+                                    <form action="{{ route('viewStudent', ['id' => $student->student_id]) }}"
+                                        method="GET">
+                                        @csrf
+                                        <input type="submit" value="VIEW" class="view form-button">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+                integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+            <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+            <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    $('#myTable').DataTable({
+                        "language": {
+                            "lengthMenu": "Display _MENU_ records per page",
+                            "zeroRecords": "No Records Available",
+                            "info": "Showing page _PAGE_ of _PAGES_",
+                            "infoEmpty": "No records available",
+                            "infoFiltered": "(filtered from _MAX_ total records)"
+                        }
+                    });
                 });
-            });
-        </script>
-    </div>
+            </script>
+        </div>
+    </section>
 @endsection
