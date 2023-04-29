d2a456a21f7f60a36b40b2f304c5673e87eccba4
rttn-Mango <khimmanga445@gmail.com>
2023-04-29 19:45:26 +0800

    Fixed home page for admin accounts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,28 +2,28 @@
 @extends('layouts.header')
 
 @section('content')
-    <div class="container">
+    <section class="view-container">
         <h2 class="text-center">REGISTRAR RECORS AND DOCUMENT MANAGEMENT SYSTEM</h2>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-4">
-            <a href="/stud_cred_mngmnt">
-                <img src="{{ asset('img/option1.jpg') }}" width="250px" height="250px">
-            </a>
+        <div class="grid-container admin-view">
+            <div>
+                <a href="/stud_cred_mngmnt">
+                    <img src="{{ asset('img/option1.jpg') }}" width="250px" height="250px">
+                </a>
+            </div>
+            @if (Auth::user()->account_role == 'rec_assoc')
+                <div>
+                    <a href="/archived_records">
+                        <img src="{{ asset('img/option3.jpg') }}" width="250px" height="250px">
+                    </a>
+                </div>
+            @endif
+            @if (Auth::user()->account_role == 'cic')
+                <div>
+                    <a href="{{ route('cic.request') }}">
+                        <img src="{{ asset('img/option2.jpg') }}" width="250px" height="250px">
+                    </a>
+                </div>
+            @endif
         </div>
-        @if (Auth::user()->account_role == 'rec_assoc')
-            <div class="col-4">
-                <a href="/archived_records">
-                    <img src="{{ asset('img/option3.jpg') }}" width="250px" height="250px">
-                </a>
-            </div>
-        @endif
-        @if (Auth::user()->account_role == 'cic')
-            <div class="col-4">
-                <a href="{{ route('cic.request') }}">
-                    <img src="{{ asset('img/option2.jpg') }}" width="250px" height="250px">
-                </a>
-            </div>
-        @endif
-    </div>
+    </section>
 @endsection
