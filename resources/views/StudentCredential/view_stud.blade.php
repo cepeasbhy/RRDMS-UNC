d6faf920a4699e1e311cd3035eeabe5ee117950b
rttn-Mango <khimmanga445@gmail.com>
2023-04-29 21:15:03 +0800

    added left-side border to the SCM header for cic

--- resources/views/StudentCredential/view_stud.blade.php
+++ resources/views/StudentCredential/view_stud.blade.php
@@ -2,54 +2,61 @@
 
 @section('content')
     <div class="row mb-3 mt-3">
-        <form class="mb-3" action="{{route('StudCredHome')}}" method="get">
+        <form class="mb-3" action="{{ route('StudCredHome') }}" method="get">
             <button class="btn btn-success btn-sm"><i class="bi bi-arrow-bar-left"></i> BACK</button>
         </form>
         <div class="col">
             <div class="border-start border-danger border-4">
                 <h4 class="ms-3">STUDENT INFORMATION</h4>
             </div>
-            <span class="badge bg-success mb-2">{{session('msg')}}</span>
+            <span class="badge bg-success mb-2">{{ session('msg') }}</span>
             <div class="ms-2 mb-3">
                 <div class="row align-items-center mb-3">
-                    <img class="col-3 img-fluid rounded-circle student-pic" data-bs-toggle="modal" data-bs-target="{{"#".$picturePath->document_id}}" src="{{asset('storage/'.$picturePath->document_loc)}}">
+                    <img class="col-3 img-fluid rounded-circle student-pic" data-bs-toggle="modal"
+                        data-bs-target="{{ '#' . $picturePath->document_id }}"
+                        src="{{ asset('storage/' . $picturePath->document_loc) }}">
                     <div class="col-9">
-                        <span class="h4 fw-bold">{{$student->last_name}}, {{$student->first_name}} {{mb_substr($student->middle_name, 0, 1).'.'}}</span>
+                        <span class="h4 fw-bold">{{ $student->last_name }}, {{ $student->first_name }}
+                            {{ mb_substr($student->middle_name, 0, 1) . '.' }}</span>
                         <br>
-                        <span>{{$student->student_id}}</span>
+                        <span>{{ $student->student_id }}</span>
                         <br>
-                        <span>{{$student->course_name}}</span>
+                        <span>{{ $student->course_name }}</span>
                     </div>
                 </div>
                 <div class="mb-2">
                     <label class="col-form-label col-form-label-sm" for="">Email</label>
-                    <input class="form-control form-control-sm" type="text" value="{{$student->email}}" readonly>
+                    <input class="form-control form-control-sm" type="text" value="{{ $student->email }}" readonly>
                 </div>
                 <div class="mb-2">
                     <label class="col-form-label col-form-label-sm" for="">Department</label>
-                    <input class="form-control form-control-sm" type="text" value="{{$student->dept_name}}" readonly>
+                    <input class="form-control form-control-sm" type="text" value="{{ $student->dept_name }}" readonly>
                 </div>
                 <div class="mb-2">
                     <label class="col-form-label col-form-label-sm" for="">Admisson Year</label>
-                    <input class="form-control form-control-sm" type="text" value="{{$student->admission_year}}" readonly>
+                    <input class="form-control form-control-sm" type="text" value="{{ $student->admission_year }}"
+                        readonly>
                 </div>
                 <div class="mb-2">
                     <label class="col-form-label col-form-label-sm" for="">Status</label>
                     @switch($student->status)
                         @case(1)
                             <input class="form-control form-control-sm" type="text" value="ACTIVE" readonly>
-                            @break
+                        @break
+
                         @case(2)
                             <input class="form-control form-control-sm" type="text" value="TRANSFERRED" readonly>
-                            @break
+                        @break
+
                         @case(3)
                             <input class="form-control form-control-sm" type="text" value="DROPPED OUT" readonly>
-                            @break
+                        @break
+
                         @default
                             <input class="form-control form-control-sm" type="text" value="GRADUATED" readonly>
                     @endswitch
                 </div>
-                @if($student->status == 4)
+                @if ($student->status == 4)
                     <div class="mb-2">
                         <label class="col-form-label col-form-label-sm" for="">Date Graduated</label>
                         <input class="form-control form-control-sm" type="text"
@@ -58,18 +65,20 @@
                 @endif
                 <div class="mb-2">
                     <label class="col-form-label col-form-label-sm" for="">Date Filed</label>
-                    <input class="form-control form-control-sm" type="text" value="{{date("Y-m-d",strtotime($student->created_at))}}" readonly>
+                    <input class="form-control form-control-sm" type="text"
+                        value="{{ date('Y-m-d', strtotime($student->created_at)) }}" readonly>
                 </div>
                 <div class="mb-3">
                     <label class="col-form-label col-form-label-sm" for="">Last Updated</label>
-                    <input class="form-control form-control-sm" type="text" value="{{date("Y-m-d",strtotime($student->updated_at))}}" readonly>
+                    <input class="form-control form-control-sm" type="text"
+                        value="{{ date('Y-m-d', strtotime($student->updated_at)) }}" readonly>
                 </div>
             </div>
             @if (Auth::user()->account_role == 'cic')
                 <div class="row g-2">
                     <div class="col">
-                        <button id="clickButton" class="btn btn-success btn-sm btn-block" 
-                        style="width: 100%" data-bs-toggle="modal" data-bs-target="#update-modal">UPDATE</button>
+                        <button id="clickButton" class="btn btn-success btn-sm btn-block" style="width: 100%"
+                            data-bs-toggle="modal" data-bs-target="#update-modal">UPDATE</button>
                     </div>
                 </div>
             @endif
@@ -78,46 +87,48 @@
             <div class="border-start border-danger border-4">
                 <h4 class="ms-3">STUDENT CREDENTIALS</h4>
             </div>
-            <span class="badge bg-success mb-2">{{session('msgCred')}}</span>
+            <span class="badge bg-success mb-2">{{ session('msgCred') }}</span>
             <div class="row">
                 @foreach ($credentials as $credential)
                     @if ($credential->document_name != 'Picture')
                         <div class="col-sm-4 mt-2">
                             <div class="card">
-                                <button class="btn p-0" data-bs-toggle="modal" data-bs-target="{{"#".$credential->document_id}}">
-                                    <img class="img-fluid p-1" src="{{asset('storage/'.$credential->document_loc)}}">
+                                <button class="btn p-0" data-bs-toggle="modal"
+                                    data-bs-target="{{ '#' . $credential->document_id }}">
+                                    <img class="img-fluid p-1" src="{{ asset('storage/' . $credential->document_loc) }}">
                                 </button>
                                 <div class="card-body text-center p-0">
-                                    <label class="col-form-label col-form-label-sm">{{$credential->document_name}}</label>
+                                    <label
+                                        class="col-form-label col-form-label-sm">{{ $credential->document_name }}</label>
                                 </div>
                             </div>
                         </div>
                     @endif
                 @endforeach
             </div>
-            @if(Auth::user()->account_role == 'cic')
+            @if (Auth::user()->account_role == 'cic')
                 <div class="col mt-3 text-center">
-                    <button class="btn btn-sm btn-success w-75" data-bs-toggle="modal" 
-                    data-bs-target="#add-single-rec">ADD A RECORD</button>
+                    <button class="btn btn-sm btn-success w-75" data-bs-toggle="modal" data-bs-target="#add-single-rec">ADD
+                        A RECORD</button>
                 </div>
             @endif
         </div>
     </div>
-        <!--Modal for Updating Student Information-->
-        @extends('layouts.modals.updateModal', ['routeName' => 'updateStudent', 'staff' => $staff])
-        <!--Modal for Deleting Student-->
-        @extends('layouts.modals.deleteModal', ['routeName' => 'deleteStudent', 'word' => 'records'])
-        <!--Modal for Viewing Credential-->
-        @extends('layouts.modals.viewCredModal', ['fromRequestedView' => false])
-        <!--Modal for deleting Credential-->
-        @extends('layouts.modals.deleteCredModal', ['routeName' => 'deleteCred'])
-        <!--Modal for updating Credential-->
-        @extends('layouts.modals.updateCredModal', ['routeName' => 'updateCred'])
-        <!--Modal for adding a Credential-->
-        @extends('layouts.modals.addSingleRecModal', ['routeName' => 'addSingleRec'])
-    @if(Session::has('errors'))
+    <!--Modal for Updating Student Information-->
+    @extends('layouts.modals.updateModal', ['routeName' => 'updateStudent', 'staff' => $staff])
+    <!--Modal for Deleting Student-->
+    @extends('layouts.modals.deleteModal', ['routeName' => 'deleteStudent', 'word' => 'records'])
+    <!--Modal for Viewing Credential-->
+    @extends('layouts.modals.viewCredModal', ['fromRequestedView' => false])
+    <!--Modal for deleting Credential-->
+    @extends('layouts.modals.deleteCredModal', ['routeName' => 'deleteCred'])
+    <!--Modal for updating Credential-->
+    @extends('layouts.modals.updateCredModal', ['routeName' => 'updateCred'])
+    <!--Modal for adding a Credential-->
+    @extends('layouts.modals.addSingleRecModal', ['routeName' => 'addSingleRec'])
+    @if (Session::has('errors'))
         <script>
-            window.onload = function(){
+            window.onload = function() {
                 document.getElementById('clickButton').click();
             }
         </script>
