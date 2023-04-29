9cf5c993ae2cabb29164f5e6dfba72bb9420e337
rttn-Mango <khimmanga445@gmail.com>
2023-04-29 23:08:35 +0800

    Fixed view page for SCM cic side

--- resources/views/StudentCredential/view_stud.blade.php
+++ resources/views/StudentCredential/view_stud.blade.php
@@ -1,119 +1,121 @@
 @extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
-    <div class="row mb-3 mt-3">
-        <form class="mb-3" action="{{ route('StudCredHome') }}" method="get">
-            <button class="btn btn-success btn-sm"><i class="bi bi-arrow-bar-left"></i> BACK</button>
-        </form>
-        <div class="col">
-            <div class="border-start border-danger border-4">
-                <h4 class="ms-3">STUDENT INFORMATION</h4>
-            </div>
-            <span class="badge bg-success mb-2">{{ session('msg') }}</span>
-            <div class="ms-2 mb-3">
-                <div class="row align-items-center mb-3">
-                    <img class="col-3 img-fluid rounded-circle student-pic" data-bs-toggle="modal"
+    <section class="main-container" style="max-width: 80%">
+        <div style="place-self: start">
+            <form class="mb-3" action="{{ route('StudCredHome') }}" method="get">
+                <button class="back view form-button"><i class="bi bi-arrow-bar-left"></i> BACK</button>
+            </form>
+        </div>
+        <span class="badge bg-success mb-2">{{ session('msg') }}</span>
+        <div class="grid-container student-information">
+            <div>
+                <h4 class="head-container request-head">STUDENT INFORMATION</h4>
+                <div class="flex-container pic-direction">
+                    <img class="profile-image view-request-val" data-bs-toggle="modal"
                         data-bs-target="{{ '#' . $picturePath->document_id }}"
                         src="{{ asset('storage/' . $picturePath->document_loc) }}">
-                    <div class="col-9">
+                    <div class="user-info">
                         <span class="h4 fw-bold">{{ $student->last_name }}, {{ $student->first_name }}
                             {{ mb_substr($student->middle_name, 0, 1) . '.' }}</span>
-                        <br>
                         <span>{{ $student->student_id }}</span>
-                        <br>
                         <span>{{ $student->course_name }}</span>
                     </div>
                 </div>
-                <div class="mb-2">
-                    <label class="col-form-label col-form-label-sm" for="">Email</label>
-                    <input class="form-control form-control-sm" type="text" value="{{ $student->email }}" readonly>
-                </div>
-                <div class="mb-2">
-                    <label class="col-form-label col-form-label-sm" for="">Department</label>
-                    <input class="form-control form-control-sm" type="text" value="{{ $student->dept_name }}" readonly>
-                </div>
-                <div class="mb-2">
-                    <label class="col-form-label col-form-label-sm" for="">Admisson Year</label>
-                    <input class="form-control form-control-sm" type="text" value="{{ $student->admission_year }}"
-                        readonly>
-                </div>
-                <div class="mb-2">
-                    <label class="col-form-label col-form-label-sm" for="">Status</label>
-                    @switch($student->status)
-                        @case(1)
-                            <input class="form-control form-control-sm" type="text" value="ACTIVE" readonly>
-                        @break
 
-                        @case(2)
-                            <input class="form-control form-control-sm" type="text" value="TRANSFERRED" readonly>
-                        @break
-
-                        @case(3)
-                            <input class="form-control form-control-sm" type="text" value="DROPPED OUT" readonly>
-                        @break
-
-                        @default
-                            <input class="form-control form-control-sm" type="text" value="GRADUATED" readonly>
-                    @endswitch
-                </div>
-                @if ($student->status == 4)
-                    <div class="mb-2">
-                        <label class="col-form-label col-form-label-sm" for="">Date Graduated</label>
-                        <input class="form-control form-control-sm" type="text"
-                            value="{{ date('Y-m-d', strtotime($student->date_graduated)) }}" readonly>
+                <div class="flex-container inner">
+                    <div class="readonly-container">
+                        <label class="col-form-label col-form-label-sm" for="">Email</label>
+                        <input class="readonly-box" type="text" value="{{ $student->email }}" readonly>
                     </div>
-                @endif
-                <div class="mb-2">
-                    <label class="col-form-label col-form-label-sm" for="">Date Filed</label>
-                    <input class="form-control form-control-sm" type="text"
-                        value="{{ date('Y-m-d', strtotime($student->created_at)) }}" readonly>
-                </div>
-                <div class="mb-3">
-                    <label class="col-form-label col-form-label-sm" for="">Last Updated</label>
-                    <input class="form-control form-control-sm" type="text"
-                        value="{{ date('Y-m-d', strtotime($student->updated_at)) }}" readonly>
+                    <div class="readonly-container">
+                        <label class="col-form-label col-form-label-sm" for="">Department</label>
+                        <input class="readonly-box" type="text" value="{{ $student->dept_name }}" readonly>
+                    </div>
+                    <div class="readonly-container">
+                        <label class="col-form-label col-form-label-sm" for="">Admisson Year</label>
+                        <input class="readonly-box" type="text" value="{{ $student->admission_year }}" readonly>
+                    </div>
+                    <div class="readonly-container">
+                        <label class="col-form-label col-form-label-sm" for="">Status</label>
+                        @switch($student->status)
+                            @case(1)
+                                <input class="readonly-box" type="text" value="ACTIVE" readonly>
+                            @break
+
+                            @case(2)
+                                <input class="readonly-box" type="text" value="TRANSFERRED" readonly>
+                            @break
+
+                            @case(3)
+                                <input class="readonly-box" type="text" value="DROPPED OUT" readonly>
+                            @break
+
+                            @default
+                                <input class="readonly-box" type="text" value="GRADUATED" readonly>
+                        @endswitch
+                    </div>
+                    @if ($student->status == 4)
+                        <div class="readonly-container">
+                            <label class="col-form-label col-form-label-sm" for="">Date Graduated</label>
+                            <input class="readonly-box" type="text"
+                                value="{{ date('Y-m-d', strtotime($student->date_graduated)) }}" readonly>
+                        </div>
+                    @endif
+                    <div class="readonly-container">
+                        <label class="col-form-label col-form-label-sm" for="">Date Filed</label>
+                        <input class="readonly-box" type="text"
+                            value="{{ date('Y-m-d', strtotime($student->created_at)) }}" readonly>
+                    </div>
+                    <div class="readonly-container">
+                        <label class="col-form-label col-form-label-sm" for="">Last Updated</label>
+                        <input class="readonly-box" type="text"
+                            value="{{ date('Y-m-d', strtotime($student->updated_at)) }}" readonly>
+                    </div>
+                    @if (Auth::user()->account_role == 'cic')
+                        <div class="form-button-container">
+                            <button id="clickButton" class="print" style="width: 70%" data-bs-toggle="modal"
+                                data-bs-target="#update-modal">UPDATE</button>
+                        </div>
+                    @endif
                 </div>
             </div>
-            @if (Auth::user()->account_role == 'cic')
-                <div class="row g-2">
-                    <div class="col">
-                        <button id="clickButton" class="btn btn-success btn-sm btn-block" style="width: 100%"
-                            data-bs-toggle="modal" data-bs-target="#update-modal">UPDATE</button>
-                    </div>
-                </div>
-            @endif
-        </div>
-        <div class="col mb-2">
-            <div class="border-start border-danger border-4">
-                <h4 class="ms-3">STUDENT CREDENTIALS</h4>
-            </div>
-            <span class="badge bg-success mb-2">{{ session('msgCred') }}</span>
-            <div class="row">
-                @foreach ($credentials as $credential)
-                    @if ($credential->document_name != 'Picture')
-                        <div class="col-sm-4 mt-2">
-                            <div class="card">
+
+            <div>
+                <h4 class="head-container request-head">STUDENT CREDENTIALS</h4>
+                <span class="badge bg-success mb-2">{{ session('msgCred') }}</span>
+                <div class="flex-container inner outer-cred-card">
+                    @foreach ($credentials as $credential)
+                        @if ($credential->document_name != 'Picture')
+                            <div class="cred-card">
                                 <button class="btn p-0" data-bs-toggle="modal"
                                     data-bs-target="{{ '#' . $credential->document_id }}">
                                     <img class="img-fluid p-1" src="{{ asset('storage/' . $credential->document_loc) }}">
                                 </button>
-                                <div class="card-body text-center p-0">
+                                <div style="text-align: center;">
                                     <label
                                         class="col-form-label col-form-label-sm">{{ $credential->document_name }}</label>
                                 </div>
                             </div>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-            @if (Auth::user()->account_role == 'cic')
-                <div class="col mt-3 text-center">
-                    <button class="btn btn-sm btn-success w-75" data-bs-toggle="modal" data-bs-target="#add-single-rec">ADD
-                        A RECORD</button>
+                        @endif
+                    @endforeach
                 </div>
-            @endif
+                @if (Auth::user()->account_role == 'cic')
+                    <div class="form-button-container">
+                        <button class="print" style="width: 70%" data-bs-toggle="modal"
+                            data-bs-target="#add-single-rec">ADD
+                            A RECORD</button>
+                    </div>
+                @endif
+            </div>
+
         </div>
-    </div>
+
+    </section>
+
+
+
     <!--Modal for Updating Student Information-->
     @extends('layouts.modals.updateModal', ['routeName' => 'updateStudent', 'staff' => $staff])
     <!--Modal for Deleting Student-->
