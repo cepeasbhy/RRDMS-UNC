90bf3777b4be8bb1ea349a907db366652670bd6c
rttn-Mango <khimmanga445@gmail.com>
2023-05-02 21:34:34 +0800

    Fixed archived records page for records associate

--- resources/views/ArchivedRecords/index.blade.php
+++ resources/views/ArchivedRecords/index.blade.php
@@ -6,66 +6,69 @@
 @endsection
 
 @section('content')
-    <section class="my-3 row align-items-center">
-        <div class="col-sm-6">
-            <h3>Archived Records Management</h3>
-            <span class="badge bg-success mb-2">{{ session('msg') }}</span>
+    <section class="main-container" style="max-width: 80%; margin-top: 3rem">
+        <div class="flex-container archive-top-container" style="justify-content: space-between">
+            <div>
+                <h3 class="head-container request-head">Archived Records Management</h3>
+                <span class="badge bg-success mb-2">{{ session('msg') }}</span>
+            </div>
+            <div class="flex-container archive-button-orientation">
+                <form class="w-100" action="{{ route('toBeArchived') }}" method="get">
+                    <input class="print" style="padding-block: 0.15rem" type="submit" value="ARCHIVE A RECORD">
+                </form>
+                <form class="w-100" action="{{ route('getRequests') }}" method="get">
+                    <input class="cancel" type="submit" value="VIEW REQUESTS">
+                </form>
+            </div>
         </div>
-        <div class="col-sm-3">
-            <form class="w-100" action="{{ route('toBeArchived') }}" method="get">
-                <input class="w-100 btn btn-sm btn-success" type="submit" value="ARCHIVE A RECORD">
-            </form>
-        </div>
-        <div class="col-sm-3">
-            <form class="w-100" action="{{ route('getRequests') }}" method="get">
-                <input class="w-100 btn btn-sm btn-danger" type="submit" value="VIEW REQUESTS">
-            </form>
-        </div>
-    </section>
 
-    <section class="container my-3">
-        <table id="archivedRecordsTable" class="table table-striped" style="width: 100%">
-            <thead>
-                <th class="custom-th bg-danger">STUDENT ID</th>
-                <th class="custom-th bg-danger">FIRST NAME</th>
-                <th class="custom-th bg-danger">LAST NAME</th>
-                <th class="custom-th bg-danger">DEPARTMENT</th>
-                <th class="custom-th bg-danger">PROGRAM</th>
-                <th class="custom-th bg-danger">ACTION</th>
-            </thead>
-            <tbody>
-                @foreach ($students as $student)
-                    <tr class="custom-tr">
-                        <td class="custom-td">{{ $student->student_id }}</td>
-                        <td class="custom-td">{{ $student->first_name }}</td>
-                        <td class="custom-td">{{ $student->last_name }}</td>
-                        <td class="custom-td">{{ $student->dept_name }}</td>
-                        <td class="custom-td">{{ $student->course_name }}</td>
-                        <td class="custom-td">
-                            <form action="{{ route('checkRecord', ['id' => $student->student_id]) }}" method="GET">
-                                @csrf
-                                <input type="submit" value="VIEW" class="btn btn-success btn-sm">
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-        </table>
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-            integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function() {
-                $('#archivedRecordsTable').DataTable({
-                    "language": {
-                        "lengthMenu": "Display _MENU_ records per page",
-                        "zeroRecords": "No records available",
-                        "info": "Showing page _PAGE_ of _PAGES_",
-                        "infoEmpty": "No records available",
-                        "infoFiltered": "(filtered from _MAX_ total records)"
-                    }
-                });
-            });
-        </script>
+        <div class="tab-container">
+            <div class="tab-container__contents active">
+                <table id="archivedRecordsTable" style="width: 100%">
+                    <thead>
+                        <th class="table-header">STUDENT ID</th>
+                        <th class="table-header">FIRST NAME</th>
+                        <th class="table-header">LAST NAME</th>
+                        <th class="table-header">DEPARTMENT</th>
+                        <th class="table-header">PROGRAM</th>
+                        <th class="table-header">ACTION</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($students as $student)
+                            <tr>
+                                <td data-label="Student Id">{{ $student->student_id }}</td>
+                                <td data-label="First Name">{{ $student->first_name }}</td>
+                                <td data-label="Last Name">{{ $student->last_name }}</td>
+                                <td data-label="Department">{{ $student->dept_name }}</td>
+                                <td data-label="Program">{{ $student->course_name }}</td>
+                                <td data-label="Action">
+                                    <form action="{{ route('checkRecord', ['id' => $student->student_id]) }}"
+                                        method="GET">
+                                        @csrf
+                                        <input type="submit" value="VIEW" class="form-button view">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                </table>
+                <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+                    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+                <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+                <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        $('#archivedRecordsTable').DataTable({
+                            "language": {
+                                "lengthMenu": "Display _MENU_ records per page",
+                                "zeroRecords": "No records available",
+                                "info": "Showing page _PAGE_ of _PAGES_",
+                                "infoEmpty": "No records available",
+                                "infoFiltered": "(filtered from _MAX_ total records)"
+                            }
+                        });
+                    });
+                </script>
+            </div>
+        </div>
     </section>
 @endsection
