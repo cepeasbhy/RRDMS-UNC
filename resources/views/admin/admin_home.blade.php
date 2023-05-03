f3c27351a1ddc09e28e83a3e9bf16a7f61c1fbea
rttn-Mango <khimmanga445@gmail.com>
2023-05-03 22:40:07 +0800

    Fixed superadmin homepage layout

--- resources/views/admin/admin_home.blade.php
+++ resources/views/admin/admin_home.blade.php
@@ -2,168 +2,159 @@
 @extends('layouts.header')
 
 @section('content')
-    <div class="row justify-content-center mb-3">
-        <div class="row">
-            <div class="border-start border-danger border-4 mb-2">
-                <h4 class="ms-1 my-auto">ADMIN TOOLS</h4>
-                <span class="badge bg-danger mb-2">{{ session('errorMsg') }}</span>
-                <span class="badge bg-success mb-2">{{ session('successMsg') }}</span>
+    <section class="main-container" style="max-width: 80%; margin-top: 3rem">
+        <h4 class="head-container request-head">ADMIN TOOLS</h4>
+        <span class="badge bg-danger mb-2">{{ session('errorMsg') }}</span>
+        <span class="badge bg-success mb-2">{{ session('successMsg') }}</span>
+        <div class="flex-container tri-button-container">
+            <div class="print" style="text-align: center">
+                <a style="text-decoration: none; color: white" href="{{ route('admin.viewAccounts') }}"
+                    rel="noopener noreferrer">ACCOUNT
+                    MANAGEMENT</a>
             </div>
-            <div class="row">
-                <div class="col">
-                    <a class="btn btn-success btn-small w-100" href="{{route('admin.viewAccounts')}}" rel="noopener noreferrer">ACCOUNT MANAGEMENT</a>
-                </div>
-                <div class="col">
-                    <button class="btn btn-success btn-small" data-bs-toggle="modal" data-bs-target="#admin-export-grad">EXPORT LIST OF GRADUATES</button>
-                </div>
-                <div class="col">
-                    <button class="btn btn-success btn-small" data-bs-toggle="modal" data-bs-target="#admin-export-stud">EXPORT LIST OF STUDENTS</button>
-                </div>
-                <div class="col">
-                    <button class="btn btn-success btn-small" data-bs-toggle="modal" data-bs-target="#admin-rec-prices">MANAGE RECORD PRICES</button>
-                </div>
+            <div>
+                <button class="print" data-bs-toggle="modal" data-bs-target="#admin-export-grad">EXPORT LIST OF
+                    GRADUATES</button>
             </div>
-        </div>
-    </div>
-    <div class="row justify-content-center mb-3">
-        <div class="row">
-            <div class="border-start border-danger border-4 mb-2">
-                <h4 class="ms-1 my-auto">RECORDS OVERVIEW</h4>
+            <div>
+                <button class="print" data-bs-toggle="modal" data-bs-target="#admin-export-stud">EXPORT LIST OF
+                    STUDENTS</button>
+            </div>
+            <div>
+                <button class="print" data-bs-toggle="modal" data-bs-target="#admin-rec-prices">MANAGE RECORD
+                    PRICES</button>
             </div>
         </div>
-        <div class="row mb-2">
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '001'])}}">
-                            <img src="{{asset('/img/dept_logo/as_logo.png')}}" height="50px" width="50px"> 
-                        </a>
-                    </div>
-                    <div class="col">
-                        <h6>Arts and Science</h6>
-                        <h5>{{$deptCount['asCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
-                </div>
+
+        <div style="margin-top: 2rem">
+            <div class="head-container request-head">
+                <h4>RECORDS OVERVIEW</h4>
             </div>
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '002'])}}">
-                            <img src="{{asset('/img/dept_logo/cba_logo.png')}}" height="50px" width="50px">
-                        </a>
-                    </div>
-                    <div class="col">
-                        <h6>Business and Accountancy</h6>
-                        <h5>{{$deptCount['cbaCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
-                </div>
-            </div>
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '003'])}}">
-                            <img src="{{asset('/img/dept_logo/cs_logo.png')}}" height="50px" width="50px">
-                        </a>
-                    </div>
-                    <div class="col">
-                        <h6>Computer Studies</h6>
-                        <h5>{{$deptCount['csCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '004'])}}">
-                            <img src="{{asset('/img/dept_logo/cje_logo.png')}}" height="50px" width="50px">
-                        </a>
+            <div class="grid-container wide-gap grid-departments" style="width: 100%">
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '001']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/as_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Arts and Science</h6>
+                                <h5>{{ $deptCount['asCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
                         </div>
-                    <div class="col">
-                        <h6>Criminal Justice Education</h6>
-                        <h5>{{$deptCount['cjeCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
+                    </a>
                 </div>
-            </div>
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '005'])}}">
-                            <img src="{{asset('/img/dept_logo/educ_logo.png')}}" height="50px" width="50px">
-                        </a>
-                    </div>
-                    <div class="col">
-                        <h6>Education</h6>
-                        <h5>{{$deptCount['educCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '002']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/cba_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Business and Accountancy</h6>
+                                <h5>{{ $deptCount['cbaCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-            </div>
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '006'])}}">
-                            <img src="{{asset('/img/dept_logo/ea_logo.png')}}" height="50px" width="50px">
-                        </a>
-                    </div>
-                    <div class="col">
-                        <span style="font-size: 15px">Engineering and Architecture</span>
-                        <h5>{{$deptCount['eaCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '003']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/cs_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Computer Studies</h6>
+                                <h5>{{ $deptCount['csCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
                 </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '004']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/cje_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Criminal Justice Education</h6>
+                                <h5>{{ $deptCount['cjeCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '005']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/educ_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Education</h6>
+                                <h5>{{ $deptCount['educCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '006']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/ea_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <span style="font-size: 15px">Engineering and Architecture</span>
+                                <h5>{{ $deptCount['eaCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '007']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/nursing_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Nursing</h6>
+                                <h5>{{ $deptCount['nursingCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '008']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/grad_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>Graduate Studies</h6>
+                                <h5>{{ $deptCount['gradCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="department-card">
+                    <a href="{{ route('admin.viewDepartment', ['deptID' => '009']) }}">
+                        <div class="flex-container department-card__contents">
+                            <img src="{{ asset('/img/dept_logo/law_logo.png') }}" height="50px" width="50px">
+                            <div class="user-info">
+                                <h6>School of Law</h6>
+                                <h5>{{ $deptCount['lawCount'] }}</h5>
+                                <label class="label-sm">TOTAL RECORDS</label>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
             </div>
         </div>
-        <div class="row mb-2">
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '007'])}}">
-                            <img src="{{asset('/img/dept_logo/nursing_logo.png')}}" height="50px" width="50px">
-                        </a>
-                        </div>
-                    <div class="col">
-                        <h6>Nursing</h6>
-                        <h5>{{$deptCount['nursingCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
-                </div>
-            </div>
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '008'])}}">
-                            <img src="{{asset('/img/dept_logo/grad_logo.png')}}" height="50px" width="50px">
-                        </a>
-                    </div>
-                    <div class="col">
-                        <h6>Graduate Studies</h6>
-                        <h5>{{$deptCount['gradCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
-                </div>
-            </div>
-            <div class="col border p-2 rounded border-dark me-2">
-                <div class="d-flex align-items-center">
-                    <div class="col-3">
-                        <a href="{{route('admin.viewDepartment', ['deptID' => '009'])}}">
-                            <img src="{{asset('/img/dept_logo/law_logo.png')}}" height="50px" width="50px">
-                        </a>
-                        </div>
-                    <div class="col">
-                        <h6>School of Law</h6>
-                        <h5>{{$deptCount['lawCount']}}</h5>
-                        <label class="label-sm">TOTAL RECORDS</label>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+    </section>
+
+
     @extends('layouts.modals.exportStudListModal')
     @extends('layouts.modals.exportGradListModal')
     @extends('layouts.modals.recPricesModal')
