2bec2b6dcc194d433f294c6f926c1c7ccd585a76
rttn-Mango <khimmanga445@gmail.com>
2023-10-08 23:03:56 +0800

    Changes: Revamped Account setup for first time login of students

--- resources/views/RequestRecord/student/change_stud_pass.blade.php
+++ resources/views/RequestRecord/student/change_stud_pass.blade.php
@@ -2,102 +2,82 @@
 @extends('layouts.header')
 
 @section('content')
-    <section class="view-container">
+    <section class="acc-setup">
+        <h1>Account Setup</h1>
+        <span style="margin-left: 1rem" class="badge bg-danger mb-2">{{ session('msg') }}</span>
 
-        <div class="form-block">
-            <div class="form-block-header">
-                <h5>ACCOUNT SETUP</h5>
-                <span class="badge bg-danger mb-2">{{ session('msg') }}</span>
+        <form method="POST" action="{{ route('stud.finishSetup') }}">
+            <h2>Secure your Account</h2>
+            @csrf
+            <div class="acc-setup__wrapper">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="email"
+                        class="@error('email') is-invalid @enderror"
+                        value="{{ old('email') }}" name="email" required placeholder="user@email.com">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="address">Current Address</label>
+                    <input id="address" type="text"
+                        class="@error('address') is-invalid @enderror"
+                        value="{{ old('address') }}" name="address" required placeholder="Jacob Street, ...">
+
+                    @error('address')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="address">Phone Number</label>
+                    <input id="phone-number" type="text"
+                        class="@error('phoneNumber') is-invalid @enderror"
+                        value="{{ old('phoneNumber') }}" name="phoneNumber" required placeholder="09...">
+
+                    @error('phoneNumber')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password">New Password</label>
+                    <input id="password" type="password"
+                        class="@error('password') is-invalid @enderror" name="password"
+                        required placeholder="Valid password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="confirm-password">{{ __('Confirm Password') }}</label>
+                    <input id="confirm-password" type="password"
+                        class="@error('password_confirm') is-invalid @enderror"
+                        name="password_confirmation" required autocomplete="current-password" placeholder="Confirm Password">
+
+                    @error('password_confirm')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <button type="submit">
+                    {{ __('Finish Account Setup') }}
+                </button>
             </div>
-
-            <div class="form-content">
-                <form method="POST" action="{{ route('stud.finishSetup') }}">
-                    @csrf
-
-                    <div class="input-group">
-                        <label for="email">Email</label>
-                        <div class="input-container">
-                            <input id="email" type="email"
-                                class="input-box form-control @error('email') is-invalid @enderror"
-                                value="{{ old('email') }}" name="email" required placeholder="user@email.com">
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="input-group">
-                        <label for="address">Current Address</label>
-                        <div class="input-container">
-                            <input id="address" type="text"
-                                class="input-box form-control @error('address') is-invalid @enderror"
-                                value="{{ old('address') }}" name="address" required placeholder="Jacob Street, ...">
-
-                            @error('address')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="input-group">
-                        <label for="address">Phone Number</label>
-                        <div class="input-container">
-                            <input id="phone-number" type="text"
-                                class="input-box form-control @error('phoneNumber') is-invalid @enderror"
-                                value="{{ old('phoneNumber') }}" name="phoneNumber" required placeholder="09...">
-
-                            @error('phoneNumber')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="input-group">
-                        <label for="password">New Password</label>
-                        <div class="input-container">
-                            <input id="password" type="password"
-                                class="input-box form-control @error('password') is-invalid @enderror" name="password"
-                                required placeholder="valid password">
-
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="input-group">
-                        <label for="confirm-password">{{ __('Confirm Password') }}</label>
-                        <div class="input-container">
-                            <input id="confirm-password" type="password"
-                                class="input-box form-control @error('password_confirm') is-invalid @enderror"
-                                name="password_confirmation" required autocomplete="current-password">
-
-                            @error('password_confirm')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-button-container">
-                        <button type="submit" class="red-button button-design" style="min-width: 35%">
-                            {{ __('Finish Account Setup') }}
-                        </button>
-                    </div>
-
-                </form>
-            </div>
-
-        </div>
+        </form>
 
     </section>
 @endsection
